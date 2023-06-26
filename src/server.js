8b1c16562dd4b23396005c7018efdf47e9fa16f5
joaozinhozerofive <124640745+joaozinhozerofive@users.noreply.github.com>
2023-06-26 19:56:08 -0300

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,27 @@
+require("express-async-errors");
+
+const AppError = require("./utils/AppError")
+const express = require('express')
+
+const routes  = require( "./routes" )
+const app = express();
+app.use(express.json());
+
+app.use(routes);
+
+
+app.use((error, request, response, next)=> {
+    if(error instanceof AppError){
+        return response.status(error.statusCode).json({
+            status: "error", 
+            messsage: error.message
+        })
+    }
+})
+
+const PORT = 3333;
+app.listen(PORT, ()=> console.log(`Server is running on PORT ${PORT}`));
+
+
+
+
